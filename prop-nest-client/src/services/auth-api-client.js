c57c883ea9171b62ddefdc21c108b08a268ac681
Farhan <farhannadim2023@gmail.com>
2026-05-03 22:05:52 +0600

    bug fix

--- prop-nest-client/src/services/auth-api-client.js
+++ prop-nest-client/src/services/auth-api-client.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const authApiClient = axios.create({
-  // baseURL: "https://propnest.onrender.com",
-  baseURL: "http://localhost:3000",
+  baseURL: "https://propnest.onrender.com",
+  // baseURL: "http://localhost:3000",
 });
 
 export default authApiClient;
